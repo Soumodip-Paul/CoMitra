1b400a1335c52540bde0c4e838c71d8be168d6dc
Soumodip Paul <soumodippaul10032002@outlook.in>
2022-01-05 20:33:10 +0530

    updated footer

--- src/components/utils/Footer.jsx
+++ src/components/utils/Footer.jsx
@@ -6,8 +6,8 @@ export const Footer = () => {
             <div>&copy; 2019–2021 CoMitra by <a href="https://github.com/Soumodip-Paul" className="text-decoration-none" target="_blank" rel="noreferrer" >Soumodip Paul</a></div>
             <div>Support Us
                 <a href="https://www.youtube.com/channel/UCOFwfN-LJ7wGvAOovAwpjxg?sub_confirmation=1" className="px-2 text-decoration-none">Youtube</a>
+                <a href="https://github.com/Soumodip-Paul" className="px-2 text-decoration-none">Github</a>
                 <a href="https://spaul10032002.blogspot.com" className="px-2 text-decoration-none">Blog</a>
-                <a href="//blog.comitra.ga" className="px-2 text-decoration-none">Website</a>
                 {/* <a href="" className="px-2 text-decoration-none">Twitter</a> */}
             </div>
         </footer>
